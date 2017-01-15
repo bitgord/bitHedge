ee381d318522a2cff3d854c643e5a3c69e860835
Michael Gord <michael.l.gord@gmail.com>
2017-01-15 10:31:36 -0500

    init repo

---
+++ public/js/price.js
@@ -0,0 +1,11 @@
+var xbtc = new XMLHttpRequest();
+xbtc.open('GET', 'https://api.bitcoinaverage.com/ticker/global/USD/', true);
+xbtc.onreadystatechange = function () {
+	if (xbtc.readyState == 4) {
+		var ticker = JSON.parse(xbtc.responseText);
+		var price = ticker.last;
+		document.getElementById('btc').innerHTML = "1 BTC = " + "$" + price;
+	}	
+};
+
+xbtc.send();
